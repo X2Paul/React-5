864a73ee82ab76dd0b1ca667866296d77355fcc3
X2Paul <paulroll27@gmail.com>
2024-04-28 21:39:17 +0300

    code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home.tsx';
-import About from './components/About.tsx';
-import Contacts from './components/Contacts.tsx';
-import Menu from './components/Menu.tsx';
-
+import { Home, About, Contacts, Menu } from './components/index.tsx';
 
 const App = () => {
-    return (
-      <BrowserRouter>
-        <Menu />
-        <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contacts" element={<Contacts/>} />
-        </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Home } from './Home.tsx';
+export { default as About } from './About.tsx';
+export { default as Contacts } from './Contacts.tsx';
+export { default as Menu } from './Menu.tsx';
