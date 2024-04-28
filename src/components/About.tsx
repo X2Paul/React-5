161ed22e31cfa0584b2243fc7b40ed71b30d7cf1
X2Paul <paulroll27@gmail.com>
2024-04-28 17:22:42 +0300

    React Router

---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <h1>Це сторінка про нас</h1>
+    )
+}
+
+export default About;
---
+++ src/components/Contacts.tsx
@@ -0,0 +1,7 @@
+const Contacts = () => {
+    return (
+        <h1>Це контактна сторінка</h1>
+    )
+}
+
+export default Contacts;
